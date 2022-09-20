370d264a2c246692b91534e7e502008043f3320e
Jim <82846092+JimCantFindAUsername@users.noreply.github.com>
2022-09-20 11:55:05 -0500

    Create Handler.lua

---
+++ Handler.lua
@@ -0,0 +1,111 @@
+
+--	          JJJJJJJJJJJ  iiii                                                    tttt                                                  	     
+--	          J:::::::::J i::::i                                                ttt:::t                                                  	     
+--	          J:::::::::J  iiii                                                 t:::::t                                                  	     
+--	          JJ:::::::JJ                                                       t:::::t                                                  	     
+--	            J:::::J  iiiiiii    mmmmmmm    mmmmmmm       ssssssssss   ttttttt:::::ttttttt        eeeeeeeeeeee    rrrrr   rrrrrrrrr   	     
+--	            J:::::J  i:::::i  mm:::::::m  m:::::::mm   ss::::::::::s  t:::::::::::::::::t      ee::::::::::::ee  r::::rrr:::::::::r  	     
+--	            J:::::J   i::::i m::::::::::mm::::::::::mss:::::::::::::s t:::::::::::::::::t     e::::::eeeee:::::eer:::::::::::::::::r 	     
+--	            J:::::j   i::::i m::::::::::::::::::::::ms::::::ssss:::::stttttt:::::::tttttt    e::::::e     e:::::err::::::rrrrr::::::r	     
+--	            J:::::J   i::::i m:::::mmm::::::mmm:::::m s:::::s  ssssss       t:::::t          e:::::::eeeee::::::e r:::::r     r:::::r	     
+--	JJJJJJJ     J:::::J   i::::i m::::m   m::::m   m::::m   s::::::s            t:::::t          e:::::::::::::::::e  r:::::r     rrrrrrr	     
+--	J:::::J     J:::::J   i::::i m::::m   m::::m   m::::m      s::::::s         t:::::t          e::::::eeeeeeeeeee   r:::::r            	     
+--	J::::::J   J::::::J   i::::i m::::m   m::::m   m::::mssssss   s:::::s       t:::::t    tttttte:::::::e            r:::::r            	     
+--	J:::::::JJJ:::::::J  i::::::im::::m   m::::m   m::::ms:::::ssss::::::s      t::::::tttt:::::te::::::::e           r:::::r            	     
+--   JJ:::::::::::::JJ   i::::::im::::m   m::::m   m::::ms::::::::::::::s       tt::::::::::::::t e::::::::eeeeeeee   r:::::r            	     
+--	   JJ:::::::::JJ     i::::::im::::m   m::::m   m::::m s:::::::::::ss          tt:::::::::::tt  ee:::::::::::::e   r:::::r            	     
+--	     JJJJJJJJJ       iiiiiiiimmmmmm   mmmmmm   mmmmmm  sssssssssss              ttttttttttt      eeeeeeeeeeeeee   rrrrrrr         
+
+
+local Buttons = script.Parent.Parent.Cosmetics.Buttons
+
+
+local 1 = Buttons.Numbers["1"]
+local 2 = Buttons.Numbers["2"]
+local 3 = Buttons.Numbers["3"]
+local 4 = Buttons.Numbers["4"]
+local 5 = Buttons.Numbers["5"]
+local 6 = Buttons.Numbers["6"]
+local 7 = Buttons.Numbers["7"]
+local 8 = Buttons.Numbers["8"]
+local 9 = Buttons.Numbers["9"]
+local 0 = Buttons.Numbers["0"]
+
+local period = Buttons.EditVariables["."]
+local negate = Buttons.EditVariables["-"]
+local fraction = Buttons.EditVariables["/"]
+
+local add = Buttons.Arithmetic["+"] 
+local sub = Buttons.Arithmetic["-"]
+local mul = Buttons.Arithmetic["x"]
+local div = Buttons.Arithmetic["/"]
+
+
+1.MouseButton1Click:Connect(function(1)
+    
+end)
+
+2.MouseButton1Click:Connect(function(2)
+    
+end)
+
+3.MouseButton1Click:Connect(function(3)
+    
+end)
+
+1.MouseButton1Click:Connect(function(4)
+    
+end)
+
+1.MouseButton1Click:Connect(function(5)
+    
+end)
+
+1.MouseButton1Click:Connect(function(6)
+    
+end)
+
+1.MouseButton1Click:Connect(function(7)
+    
+end)
+
+1.MouseButton1Click:Connect(function(8)
+    
+end)
+
+1.MouseButton1Click:Connect(function(9)
+    
+end)
+
+1.MouseButton1Click:Connect(function(0)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Period)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Negate)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Fraction)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Add)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Sub)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Mul)
+    
+end)
+
+1.MouseButton1Click:Connect(function(Div)
+    
+end)
+
